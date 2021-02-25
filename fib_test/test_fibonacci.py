8ff1e5c5de653d7ad1eaf6497cf987ed0ba2cef5
Kyle <huangkyle110@gmail.com>
2021-02-25 08:22:20 -0800

    added fibonacci unit test

---
+++ fib_test/test_fibonacci.py
@@ -0,0 +1,24 @@
+import unittest
+from fibonacci import fib
+
+
+class TestCase(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(fib(0), 0)
+
+    def test2(self):
+        self.assertEqual(fib(1), 1)
+
+    def test3(self):
+        self.assertEqual(fib(2), 1)
+
+    def test4(self):
+        self.assertEqual(fib(10), 55)
+
+    def test5(self):
+        self.assertEqual(fib(20), 6765)
+
+
+if __name__=="__main__":
+    print(fib(10))
+    unittest.main()
