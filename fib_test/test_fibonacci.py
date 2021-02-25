02b768ecb93629ecf71e85f04eea84bda862cf89
Kyle <huangkyle110@gmail.com>
2021-02-25 08:39:23 -0800

    cleaned up

--- fib_test/test_fibonacci.py
+++ fib_test/test_fibonacci.py
@@ -2,6 +2,10 @@ import unittest
 from fibonacci import fib
 
 
+def test_answer():
+    assert fib(10) == 55
+
+
 class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(fib(0), 0)
